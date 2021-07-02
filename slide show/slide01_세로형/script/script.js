159388881bc9f9ece48249372140a879aee65432
op382p <https://github.com/op382p/0630_Slide-Show.git>
2021-07-02 15:49:40 +0900

    0630_ 슬라이드 쇼

---
+++ slide show/slide01_세로형/script/script.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+$(function () {
+
+    setInterval(fnSlide, 5000);
+
+    function fnSlide() {
+        $("#shuttleFrame").animate({
+                "margin-top": "-300px"
+            },
+            3000,
+            function () {
+
+                $("#shuttleFrame>a:nth-child(1)")
+                    .insertAfter("#shuttleFrame>a:nth-child(3)");
+                $("#shuttleFrame").css({
+                    "margin-top": "0"
+                })
+            }
+        );
+    }
+});
