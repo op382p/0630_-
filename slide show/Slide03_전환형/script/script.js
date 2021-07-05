87299be7f20a36fc89b0bc9b23b0e64de93bc20f
op382p <https://github.com/op382p/0630_Slide-Show.git>
2021-07-05 15:50:51 +0900

    0630_Slide-Show

---
+++ slide show/Slide03_전환형/script/script.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+
+$(function () {
+
+            $("#slide a:fist-child").fadeOut(3000);
+
+    function ( )
+            $("#slide a:nth-child(2)").fadeIn(5000);
+
+        }
